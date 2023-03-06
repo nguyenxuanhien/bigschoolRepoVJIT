98819f6bf47d77fee236d4a0f8c3d1616a00e295
NguyenXuanHien <meke213123@gmail.com>
2023-03-06 14:52:30 +0700

    update

--- bigschool/Controllers/CoursesController.cs
+++ bigschool/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using bigschool.Models;
+using bigschool.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,19 @@ namespace bigschool.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         // GET: Courses
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModels
+            {
+                categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ bigschool/ViewModels/CourseViewModels.cs
@@ -0,0 +1,21 @@
+﻿using bigschool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bigschool.ViewModels
+{
+    public class CourseViewModels
+    {
+        public String Place { get; set; }
+        public String Date { get; set; }
+        public String Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(String.Format("{0}{1}",Date,Time));
+        }
+    }
+}
