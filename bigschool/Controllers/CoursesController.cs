655a1b7ce0746a6882cd3e93617784c20296118c
NguyenXuanHien <meke213123@gmail.com>
2023-03-08 11:33:22 +0700

    update

--- bigschool/Controllers/CoursesController.cs
+++ bigschool/Controllers/CoursesController.cs
@@ -1,10 +1,13 @@
 ﻿using bigschool.Models;
 using bigschool.ViewModels;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace bigschool.Controllers
 {
@@ -20,9 +23,31 @@ namespace bigschool.Controllers
         {
             var viewModel = new CourseViewModels
             {
-                categories = _dbContext.Categories.ToList()
+                Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
         }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModels viewModels)
+        {
+            if (!ModelState.IsValid)
+            {
+               viewModels.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModels);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModels.GetDateTime(),
+                CategoryId = viewModels.Category,
+                Place = viewModels.Place,
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
--- bigschool/ViewModels/CourseViewModels.cs
+++ bigschool/ViewModels/CourseViewModels.cs
@@ -1,6 +1,7 @@
 ﻿using bigschool.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,14 +9,20 @@ namespace bigschool.ViewModels
 {
     public class CourseViewModels
     {
+        [Required]
         public String Place { get; set; }
+        [Required]
+        [FutureDate]
         public String Date { get; set; }
+        [Required]
+        [ValidTime]
         public String Time { get; set; }
+        [Required]
         public byte Category { get; set; }
-        public IEnumerable<Category> categories { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
         {
-            return DateTime.Parse(String.Format("{0}{1}",Date,Time));
+            return DateTime.Parse(String.Format("{0} {1}", Date, Time));
         }
     }
 }
